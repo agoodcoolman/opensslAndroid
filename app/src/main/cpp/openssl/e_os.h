cf409430e7c4222ac6e80796c4be62d1d507b098
agoodcoolman <agoodcoolman@gmail.com>
2018-02-28 23:15:21 +0800

    1.复制电脑上安装过的include

--- app/src/main/cpp/openssl/e_os.h
+++ app/src/main/cpp/openssl/e_os.h
@@ -61,7 +61,7 @@
 
 # include "./openssl/opensslconf.h"
 
-# include "./e_os2.h"
+# include "./openssl/e_os2.h"
 /*
  * <openssl/e_os2.h> contains what we can justify to make visible to the
  * outside; this file e_os.h is not part of the exported interface.
