384fb49ef097fdeb5abdee5e7db9d9f45a6ff30d
agoodcoolman <agoodcoolman@gmail.com>
2018-03-06 01:09:56 +0800

    1.TODO

--- app/src/main/cpp/openssl/crypto/modify.py
+++ app/src/main/cpp/openssl/crypto/modify.py
@@ -2,19 +2,43 @@
 # 把C文件里面的openssl的路径批量修改
 import os, re, fileinput, stat
 
+lists = []
+def file_h_list(file_dir):
+    global lists
+    for root, dirs, files in os.walk(file_dir):
+        for file in files:
+            if (os.path.splitext(file)[1] == '.h') :
+                lists.append(file)
+
+
+        for dir in dirs:
+            file_h_list(os.path.join(root, dir))
+    return list
 
 def file_name(file_dir):
     global L;
 
     for root, dirs, files in os.walk(file_dir):
         for file in files:
-            if (os.path.splitext(file)[1] == '.c'):
-
-                modify2(os.path.join(root, file))
+            if (os.path.splitext(file)[1] == '.h')| (os.path.splitext(file)[1] == '.c'):
+                modify4(os.path.join(root, file))
         for dir in dirs:
             file_name(os.path.join(root, dir))
 
+def modify4(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    #写的方式打开文件
+    with open(file, 'w') as f_w:
+        for line in lines:
+            results = re.compile(r'#\s*include\s*[\<\"](.*)[\>\"]').findall(line)
+        // TODO
+            for result in results:
+                if result in lists:
+                    line = re.sub(r'[\<\"]' '\"../../openssl/')
+                    line.replace('>', '\"')
 
+            f_w.write(line)
 
 def modify2(file):
 
@@ -27,6 +51,19 @@ def modify2(file):
                             line = re.sub('#\s*include\s*(\<).*openssl', '\"', line)
                             line = line.replace('>', '\"')
 
+            f_w.write(line)
+#
+def modify3(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+    #写的方式打开文件
+    with open(file, 'w') as f_w:
+        for line in lines:
+            results = re.compile(r'#\s*include\s*\"(.*)\"').findall(line)
+            for result in results:
+
+                if result in lists:
+                    line = line.replace('<', '"../../openssl/').replace('>', '\"')
             f_w.write(line)
 
 def modify(file):
@@ -49,5 +86,7 @@ def modify(file):
     finally:
         file_object1.close()
 
+file_h_list(os.getcwd())
 file_name(os.getcwd())
+
 #modify2('/root/桌面/opensslAndroid/app/src/main/cpp/openssl/crypto/asn1/n_pkey.c')
