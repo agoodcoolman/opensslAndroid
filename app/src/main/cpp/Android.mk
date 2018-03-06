6eb94a332d6723c4ddae2e31d800043c3c865bb7
DESKTOP-TTIC7FS\jin <agoodcoolman@gmail.com>
2018-03-06 15:43:07 +0800

    1.编译 报错 文件不报错了

--- app/src/main/cpp/Android.mk
+++ app/src/main/cpp/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 #to the Android.mk file and it should work.  As explained in the ndk doc, it is because the ndk performs a debug checking even for avoiding runtime linking error and for some motivations it doesn't find the correct references in a pre-built library. Hope it coulds help.
-LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
+#LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
 
 subdirs := $(addprefix $(LOCAL_PATH)/,$(addsuffix /Android.mk, \
 		openssl/ssl \
